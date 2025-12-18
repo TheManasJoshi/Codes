0eb1991ed40aead8e3d0da2fcd84db2e210e2082
TheManasJoshi <joshimanas904@gmail.com>
2025-12-18 11:22:56 +0530

    Org Folder

--- DSA/Deque/DequeBasic.java
+++ DSA/Deque/DequeBasic.java
@@ -5,7 +5,7 @@ class DequeBasic {
         Scanner sc = new Scanner(System.in);
         Deque<Integer> dq = new ArrayDeque<>();
 
-        // Adding elements at both ends
+        
         dq.addFirst(10);
         dq.addLast(20);
         dq.addFirst(5);
@@ -13,11 +13,11 @@ class DequeBasic {
 
         System.out.println("Deque: " + dq);
 
-        // Removing elements
+       
         System.out.println("Removed First: " + dq.removeFirst());
         System.out.println("Removed Last: " + dq.removeLast());
 
-        // Peek
+        
         System.out.println("Peek First: " + dq.peekFirst());
         System.out.println("Peek Last: " + dq.peekLast());
 
